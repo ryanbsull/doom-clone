ab6266a7986be952a632f51cccafc13157c57cb2
Ryan B Sullivan <ryanbakersullivan@gmail.com>
2024-12-16 10:37:49 -0700

    doom-clone: init project structure
    Signed-off-by: Ryan B Sullivan <ryanbakersullivan@gmail.com>

---
+++ src/doom-clone.c
@@ -0,0 +1,13 @@
+#include "SDL2/SDL_render.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+#define SCREEN_WIDTH	640
+#define SCREEN_HEIGHT	400
+
+int main() {
+	return 0;
+}
