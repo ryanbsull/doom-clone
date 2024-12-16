41cb7e851845692dbd4f8ee16aea82ed97e9c73e
Ryan B Sullivan <ryanbakersullivan@gmail.com>
2024-12-16 10:48:41 -0700

    doom-clone: add in SDL initialization and cleanup
    Signed-off-by: Ryan B Sullivan <ryanbakersullivan@gmail.com>

--- src/doom-clone.c
+++ src/doom-clone.c
@@ -45,7 +45,59 @@ struct {
 	player player;
 } state;
 
+int init();
+int game_loop();
+int cleanup();
+
 int main() {
 	printf("DOOM\n");
+	if (init())
+		return 1;
+	while (!game_loop()) {
+		SDL_Delay(5);
+	}
+	return cleanup();
+}
+
+int init() {
+	state.win = SDL_CreateWindow(
+		"raycast_test",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH, SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN
+		);
+	if (!state.win) {
+		printf("Failed to create window:\n\tError: %s", SDL_GetError());
+		return 1;
+	}
+
+	state.renderer = SDL_CreateRenderer(state.win, -1, SDL_RENDERER_PRESENTVSYNC);
+	if (!state.renderer) {
+		printf("Failed to create renderer:\n\tError: %s", SDL_GetError());
+		return 1;
+	}
+
+	state.texture = SDL_CreateTexture(
+		state.renderer, 
+		SDL_PIXELFORMAT_ABGR8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		SCREEN_WIDTH, SCREEN_HEIGHT
+		);
+	if (!state.texture) {
+		printf("Failed to create texture:\n\tError: %s", SDL_GetError());
+		return 1;
+	}
+	return 0;
+}
+
+int game_loop() {
+	return 1;
+}
+
+int cleanup() {
+	SDL_DestroyTexture(state.texture);
+	SDL_DestroyRenderer(state.renderer);
+	SDL_DestroyWindow(state.win);
+	SDL_Quit();
 	return 0;
 }
