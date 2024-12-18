9273dd1b108afa1e84a33a157469eaebbb3651cb
Ryan B Sullivan <ryanbakersullivan@gmail.com>
2024-12-17 19:14:50 -0700

    doom-clone: add in ability to draw textured ray to screen for when raycasting is implemented
    Signed-off-by: Ryan B Sullivan <ryanbakersullivan@gmail.com>

--- src/display.h
+++ src/display.h
@@ -3,6 +3,7 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <cstdint>
 
 #define SCREEN_WIDTH		640
 #define SCREEN_HEIGHT		400
@@ -23,8 +24,20 @@ int init_textures() {
 	return 0;
 }
 
-int draw_ray(int x, int start, int end, void* pixels, void* texture, int tex_idx) {
-	for (int i = start; i < end; i++);
+int draw_ray(uint32_t* pixels, int x, int tex_x, int start, int end, int idx, int side) {
+	int y, tex_y, tex_idx = idx * TEXTURE_HEIGHT * TEXTURE_WIDTH;
+	uint32_t color;
+	float step = TEXTURE_HEIGHT / (float)(end - start);
+	float tex_pos = (float)(start) - ((float)SCREEN_HEIGHT / 2) + ((float)(end - start) / 2) * step;
+
+	for (int i = start; i < end; i++) {
+		tex_y = (int) tex_pos & (TEXTURE_HEIGHT - 1);
+		tex_pos += step;
+		color = textures->pixels[tex_idx + TEXTURE_HEIGHT * tex_y + tex_x];
+		color = (side == x_side) ? (color >> 1) & 8355711 : color;
+		pixels[y * SCREEN_WIDTH + x] = color;
+	}
+
 	return 0;
 }
 
